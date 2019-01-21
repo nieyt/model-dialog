547c3ddd3289b32d6c0478444952cfd286b11c14
nieyingting <nieyingting@jd.com>
2019-01-21 19:19:10 +0800

    init

---
+++ src/model.js
@@ -0,0 +1,18 @@
+class Model{
+    constructor() {
+
+    }
+
+    view() {
+        const modelBox = document.createElement('div')
+        // modelBox.innerHTML = `<>`
+    }
+
+    alert() {
+
+    }
+
+    confirm() {
+
+    }
+}
